fa5286520fc490f6925da9cc25ae973839a4da30
这这这这么 <101029223+vavavca@users.noreply.github.com>
2023-03-29 22:50:31 +0800

    Update main.py

--- main.py
+++ main.py
@@ -38,34 +38,34 @@ def main(usr, pw):
 
 
 # 发送到我的邮箱
-def send(info, mail, receivers, subject='', imgpth=''):
-    #if info or mail[0] or mail[1] or receivers == '': return
-    sender,key = mail
-    message = MIMEMultipart('mixed')
-    if subject == "": subject = info
-    message['Subject'] = subject
-    message['From'] = sender
-    message['To'] = receivers
-    try:  # 尝试发送图片和文字
-        fp = open(imgpth, 'rb')  # 打开文件
-        msgImage = MIMEImage(fp.read())  # 创建MIMEImage对象，读取图片内容并作为参数
-        fp.close()  # 关闭文件
-        msgImage.add_header('Content-ID', '<image1>')  # 指定图片文件的Content-ID，imgid，<img>标签中的src用到
-        html_img = f'<p>{info}<br><img src="cid:image1"></br></p>'  # html格式添加图片
-        message.attach(msgImage)
-        message.attach(MIMEText(html_img, 'html', 'utf-8'))  # 添加到邮件正文
-    except:  # 只发送文字
-        content = MIMEText('%s' % info)
-        message.attach(content)
+# def send(info, mail, receivers, subject='', imgpth=''):
+#     #if info or mail[0] or mail[1] or receivers == '': return
+#     sender,key = mail
+#     message = MIMEMultipart('mixed')
+#     if subject == "": subject = info
+#     message['Subject'] = subject
+#     message['From'] = sender
+#     message['To'] = receivers
+#     try:  # 尝试发送图片和文字
+#         fp = open(imgpth, 'rb')  # 打开文件
+#         msgImage = MIMEImage(fp.read())  # 创建MIMEImage对象，读取图片内容并作为参数
+#         fp.close()  # 关闭文件
+#         msgImage.add_header('Content-ID', '<image1>')  # 指定图片文件的Content-ID，imgid，<img>标签中的src用到
+#         html_img = f'<p>{info}<br><img src="cid:image1"></br></p>'  # html格式添加图片
+#         message.attach(msgImage)
+#         message.attach(MIMEText(html_img, 'html', 'utf-8'))  # 添加到邮件正文
+#     except:  # 只发送文字
+#         content = MIMEText('%s' % info)
+#         message.attach(content)
 
-    try:
-        server = smtplib.SMTP_SSL("smtp.qq.com", 465)
-        server.login(sender, key)
-        server.sendmail(sender, receivers, message.as_string())
-        server.quit()
-        # print("send OK")
-    except smtplib.SMTPException as e:
-        print(e)
+#     try:
+#         server = smtplib.SMTP_SSL("smtp.qq.com", 465)
+#         server.login(sender, key)
+#         server.sendmail(sender, receivers, message.as_string())
+#         server.quit()
+#         # print("send OK")
+#     except smtplib.SMTPException as e:
+#         print(e)
 
 
 if __name__ == "__main__":
